152b8abee458fcb7863c40bde40d5fb8ca68c075
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2025-01-03 23:06:59 -0400

    GC - Semilla de usuario

--- seed/usuarios.js
+++ seed/usuarios.js
@@ -1,22 +1,9 @@
+import { bcrypt } from "bcrypt";
 const usuarios = [
   {
-    nombre: "Juan Pérez",
+    nombre: "Alexander Campusano",
     email: "campusanogaryp14@gmail.com",
-    password: "password123",
-    token: null,
-    confirmado: true,
-  },
-  {
-    nombre: "Maria López",
-    email: "maria.lopez@example.com",
-    password: "password456",
-    token: null,
-    confirmado: false,
-  },
-  {
-    nombre: "Carlos Gómez",
-    email: "carlos.gomez@example.com",
-    password: "password789",
+    password: bcrypt.hashSync("campusanogaryp14@gmail.com", 10),
     token: null,
     confirmado: true,
   },
