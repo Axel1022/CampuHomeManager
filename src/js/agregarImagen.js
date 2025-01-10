956cef0535b587f4a77d1b2885763e6b11f67358
Gary Alexander Campusano Paredes <ingcampusano.outlook.com>
2025-01-10 09:45:27 -0400

    GC - Compilacion public static

--- src/js/agregarImagen.js
+++ src/js/agregarImagen.js
@@ -3,12 +3,27 @@ import { Dropzone } from "dropzone";
 Dropzone.options.imagen = {
   dictDefaultMessage: "Arrastra tus imagenes aquí",
   acceptedFiles: ".png, .jpg, .jpge",
+  autoProcessQueue: false,
   maxFilesize: 5,
   maxFiles: 1,
   parallelUploads: 1,
   addRemoveLinks: true,
   dictRemoveFile: "Eliminar archivo",
   dictMaxFilesExceeded: "Solo puede subir un archivo",
+  paramName: "imagen",
+  init: function () {
+    const dropzone = this;
+    const btnPublicar = document.getElementById("publicar");
+    btnPublicar.disabled = false;
 
+    btnPublicar.addEventListener("click", function () {
+      dropzone.processQueue();
+    });
 
+    dropzone.on("queuecomplete", function () {
+      if (dropzone.getActiveFiles().length === 0) {
+        window.location.href = "/propiedades";
+      }
+    });
+  },
 };
